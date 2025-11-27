2583d8b9a405ef34e7be594a56ca657a5c796c36
matteovillano <villano.1840807@studenti.uniroma1.it>
2025-11-27 05:36:35 +0000

    ops

---
+++ ops.c
@@ -0,0 +1,51 @@
+#include "ops.h"
+
+int create(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int chmod(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int move(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int upload(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int download(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int cd(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int list(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int read(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int write(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
+
+int del(int id, DIR* dir, char* command) {
+    
+    return 0;
+}
--- ops.h
+++ ops.h
@@ -1,10 +1,17 @@
-int create();
-int chmod();
-int move();
-int upload();
-int download();
-int cd();
-int list();
-int read();
-int write();
-int del();
+#ifndef OPS_H
+#define OPS_H
+
+#include <dirent.h>
+
+int create(int id, DIR* dir, char* command);
+int chmod(int id, DIR* dir, char* command);
+int move(int id, DIR* dir, char* command);
+int upload(int id, DIR* dir, char* command);
+int download(int id, DIR* dir, char* command);
+int cd(int id, DIR* dir, char* command);
+int list(int id, DIR* dir, char* command);
+int read(int id, DIR* dir, char* command);
+int write(int id, DIR* dir, char* command);
+int del(int id, DIR* dir, char* command);
+
+#endif
